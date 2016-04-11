81683fe870e72955f7e080b7beaebc691921b7e8
zhangzhaopds <zhangzhaopds@foxmail.com>
2016-04-11 15:47:52 +0800

    Initial Commit

---
+++ UUID/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UUID
+//
+//  Created by 张昭 on 16/4/11.
+//  Copyright © 2016年 张昭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UUID/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UUID
+//
+//  Created by 张昭 on 16/4/11.
+//  Copyright © 2016年 张昭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
